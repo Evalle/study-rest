22bf29a32ebc2feb7e1f2a74ed1e153a7ee2f86b
evalle <shmarnev@gmail.com>
2017-03-27 21:03:00 +0200

    Add jsonify import

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -22,7 +22,7 @@ tasks = [
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jnotify({'tasks': tasks})
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
