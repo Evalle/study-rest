63383de9a2a42fe63e76166b72ffee884e23fcf1
evalle <shmarnev@gmail.com>
2017-03-29 13:52:52 +0200

    Improve web service interface

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -20,9 +20,19 @@ tasks = [
 ]
 
 
+def make_public_task(task):
+    new_task = dict()
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
